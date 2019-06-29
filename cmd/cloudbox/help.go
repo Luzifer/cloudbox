4e85b792b8ed039deccb43b4b266116b28dddf52
Knut Ahlers <knut@ahlers.me>
2019-06-29 22:38:00 +0200

    Add share command to help
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- cmd/cloudbox/help.go
+++ cmd/cloudbox/help.go
@@ -9,6 +9,7 @@ import (
 const helpText = `
 Available commands:
   help            Display this message
+  share           Shares a file and returns its URL when supported
   sync            Executes the bi-directional sync
   write-config    Write a sample configuration to specified location
 `
